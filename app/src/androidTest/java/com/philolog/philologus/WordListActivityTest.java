ac5d40919a7a2cddc0fd10aafaefde9d3b005348
jeremymarch <jmarch@gradcenter.cuny.edu>
2026-01-17 10:11:34 -0500

    github workflow tests: increase sleep to let database install before testing

--- app/src/androidTest/java/com/philolog/philologus/WordListActivityTest.java
+++ app/src/androidTest/java/com/philolog/philologus/WordListActivityTest.java
@@ -32,7 +32,7 @@ public class WordListActivityTest {
         // We use a simple sleep here if idling resources aren't implemented, 
         // but checking for isDisplayed() on the list is usually enough for Espresso to wait.
         // However, given the focus error, let's give it a moment to settle.
-        Thread.sleep(5000); 
+        Thread.sleep(8000);
 
         onView(withId(android.R.id.list)).check(matches(isDisplayed()));
 
